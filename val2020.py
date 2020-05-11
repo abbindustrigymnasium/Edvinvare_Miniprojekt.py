7cab24de9537cddf52819d4c4eaf1e9b0e20914f
abbedvvar <edvin.vare@abbindustrigymnasium.se>
2020-05-11 08:55:52 +0200

    Kommentarer i koden

--- val2020.py
+++ val2020.py
@@ -1,12 +1,12 @@
-import numpy
+import numpy        #importing numpy for a randomizer with max and min value
 
-Partier = [  
+Partier = [         #Defining all parties in a list of dictionaries
     {
-        "namn" : "Gröngöligarna",
-        "inriktning" : "Vänster",
-        "block" : "Småpartierna",
-        "min_röst" : 3,
-        "max_röst" : 12,
+        "namn" : "Gröngöligarna",       #Name of the party
+        "inriktning" : "Vänster",       #Political ideology
+        "block" : "Småpartierna",       #Political coalition
+        "min_röst" : 3,                 #Minimum availible votes
+        "max_röst" : 12,                #Maximum available votes
     },
         {
         "namn" : "Partikelpartiet",
@@ -58,7 +58,7 @@ Partier = [
         "max_röst" : 34,
     }]
 
-slutres = 0
+slutres = 0                 #Defining all variables used in the code
 nyres = slutres
 mandat = 0
 just = 0
@@ -66,58 +66,58 @@ Oljeblocket = 0
 Småpartierna = 0
 hproc = 0
 vproc = 0
-röstberättigade = 0
+röstberättigade = 7500000
 röstare= 0
 fuskare= 0
 stop= False
-röstberättigade=int(input("Hur många rösberättigade finns i samhället som ska rösta\n"))
-print("\n----------------------------------------------------------------------------\n")
+röstberättigade=int(input("Hur många rösberättigade finns i samhället som ska rösta\n"))        #Asking how many people that are eligeble to vote in the country
+print("\n----------------------------------------------------------------------------\n")       #Making a distinct break in the terminal for better readability
 while stop == False:
     for Parti in Partier:
-        resultat = numpy.random.randint(high= Parti["max_röst"],low= Parti["min_röst"])
-        slutres = slutres + resultat
-        Parti["resultat"] = resultat
+        resultat = numpy.random.randint(high= Parti["max_röst"],low= Parti["min_röst"])     #Randomizing result depinding on each parties special max/min votes
+        slutres = slutres + resultat        #Adding all parties results together
+        Parti["resultat"] = resultat        #Adding a new key in the dictionaries with the result
     if slutres>100:
-        slutres = 0
+        slutres = 0         #if result is over 100% it starts over
     else:
         break
 nyres=slutres-just
 
 for dic in Partier:
     if dic["resultat"]<4:
-        just=dic["resultat"]-just
-    print(dic["namn"],"fick", dic["resultat"],"%",end=" ")
+        just=dic["resultat"]-just       #Counting on wether the party makes it into the parliament
+    print(dic["namn"],"fick", dic["resultat"],"%",end=" ")      #printing the result for each party in the voting
 
     if dic["resultat"]>=4:
-        mandat= int(dic["resultat"]/nyres*349)
-        print("vilket är",mandat,"mandat i riksdagen")
+        mandat= int(dic["resultat"]/nyres*349)      #counting how many mandates the party gets in the parliament
+        print("vilket är",mandat,"mandat i riksdagen")      #printing the result of the mandates
         if dic["block"]=="Oljeblocket":
-            Oljeblocket=Oljeblocket+mandat
+            Oljeblocket=Oljeblocket+mandat      #counting mandates for each political coalition
         elif dic["block"]=="Småpartierna":
             Småpartierna=Småpartierna+mandat
     else:
         print("och kommer då inte in i riksdagen")
     
-    if dic["inriktning"]=="Höger":
+    if dic["inriktning"]=="Höger":          #counting percentage for the left or right 
         hproc=hproc+dic["resultat"]
     else:
         vproc=vproc+dic["resultat"]
 
 print("\n----------------------------------------------------------------------------\n")
 
-print("Oljeblocket får",Oljeblocket,"mandat")
-print("Småpartierna får",Småpartierna,"mandat")
+print("Oljeblocket får",Oljeblocket,"mandat")       #printing overall facts not for each party
+print("Småpartierna får",Småpartierna,"mandat")     #result of mandates for the coalitions
 
 if vproc>hproc:
-    print("Folk röstar mer mot vänster med totalt",vproc-hproc,"% enheter")
+    print("Folk röstar mer mot vänster med totalt",vproc-hproc,"% enheter")     #printing peoples voting tendencies either left or right and by how much
 elif vproc==hproc:
     print("Folk röstar lika mycket vänster som höger")
 else:
     print("Folk röstar mer mot höger med totalt",hproc-vproc,"% enheter")
-print("Valdeltagandet var",slutres,"%")
-röstare=int(röstberättigade*slutres/100)
-fuskare= röstberättigade-röstare
+print("Valdeltagandet var",slutres,"%")     #Presenting how high the voting participation was
+röstare=int(röstberättigade*slutres/100)        #calculating the number of total voters there was
+fuskare= röstberättigade-röstare        #calculating the peple who didnt vote
 if fuskare==0:
-    print("Alla röstberättigade i hela samhället röstade, hurra för demokratin!!!")
+    print("Alla röstberättigade i hela samhället röstade, hurra för demokratin!!!")     #if everybody voted this result shows
 else:
-    print("Det var",fuskare,"personer som inte röstade, fy på dem!!!")
+    print("Det var",fuskare,"personer som inte röstade, fy på dem!!!")      #if not everybody voted it shows how many didnt
